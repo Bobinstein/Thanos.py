6bc116717f239891396c9a3e7aa532c0affeeef8
giga <cavanaugh.stephen1991@gmail.com>
2022-09-14 20:02:43 -0400

    changes

--- Thanos.py
+++ Thanos.py
@@ -16,9 +16,9 @@ halfFiles = files[::2]
 
 
 print("The hardest choices require the strongest wills.")
-sleep(1.5)
+sleep(2.5)
 print("You have my respect, User. When I am done, half of your files will still exist.")
-sleep(1.5)
+sleep(2.5)
 print("*** You look into 14,000,605 possible futures. You see only 1 in which this destruction is ultimately averted.")
 sleep(2.5)
 print("*** Roll your 14,000,605 sided dice ***")
@@ -37,8 +37,9 @@ else:
     sleep(2)
     print("Fun isn't something one considers when balancing the universe. But this... Does put a smile on my face")
     for file in files:
-        # os.remove(file)
-        print(file)
+        for item in file:
+         #   os.remove(file)
+            print(file)
     sleep(2.5)
     print("Perfectly balanced, as all things should be.")
 
