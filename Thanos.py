bd697004e165c868c19cea9007bfc07e3ddbfca4
Stephen <62410139+Bobinstein@users.noreply.github.com>
2022-09-17 16:24:58 -0400

    update for testing

--- Thanos.py
+++ Thanos.py
@@ -24,17 +24,18 @@ random.shuffle(files)
 halfFiles = files[::2]
 
 
-print("The hardest choices require the strongest wills.")
-sleep(2.5)
-print("You have my respect, User. When I am done, half of your files will still exist.")
-sleep(2.5)
-print("*** You look into 14,000,605 possible futures. You see only 1 in which this destruction is ultimately averted. ***")
-sleep(3.5)
-print("*** Roll your 14,000,605 sided dice ***")
+# print("The hardest choices require the strongest wills.")
+# sleep(2.5)
+# print("You have my respect, User. When I am done, half of your files will still exist.")
+# sleep(2.5)
+# print("*** You look into 14,000,605 possible futures. You see only 1 in which this destruction is ultimately averted. ***")
+# sleep(3.5)
+# print("*** Roll your 14,000,605 sided dice ***")
 
 roll = random.randint(1, 14000605)
 
-sleep(3)
+# sleep(3)
+pprint(halfFiles)
 
 if roll == 69420:
     print("*** You rolled a", roll, "***")
